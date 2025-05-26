cf027858616d6a8dcfdeb0aab6ffef9d6e81027f
kinesis19 <lordkinesis@naver.com>
2025-05-26 22:12:08 +0900

    :package: Add: cygnus urdf package

---
+++ src/cygnus_description/build/cygnus_description/CMakeFiles/cygnus_description_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for cygnus_description_uninstall.
+# This may be replaced when dependencies are built.
---
+++ src/cygnus_description/build/cygnus_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kinesis/colcon_ws/src/cygnus_description
+# Build directory: /home/kinesis/colcon_ws/src/cygnus_description/build/cygnus_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
