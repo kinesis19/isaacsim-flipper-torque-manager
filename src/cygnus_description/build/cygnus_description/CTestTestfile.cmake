0f775c99c0a69b2c46f8db5179382181659246b0
kinesis19 <lordkinesis@naver.com>
2025-05-28 02:09:04 +0900

    :fire: Delete: useless files

--- src/cygnus_description/build/cygnus_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kinesis/colcon_ws/src/cygnus_description
-# Build directory: /home/kinesis/colcon_ws/src/cygnus_description/build/cygnus_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
